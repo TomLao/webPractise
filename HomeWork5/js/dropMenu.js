0c491aaae430a2b157ae3956d28d5630ec273783
Tom Lao <1026022953@qq.com>
2017-11-21 16:48:30 +0800

    JS二次菜单

---
+++ HomeWork5/js/dropMenu.js
@@ -0,0 +1,8 @@
+function showsub(obj){
+	var submenu=obj.getElementsByTagName('ul')[0];
+	submenu.style.display="block";
+}
+function hidesub(obj){
+	var submenu=obj.getElementsByTagName('ul')[0];
+	submenu.style.display="none";
+}
